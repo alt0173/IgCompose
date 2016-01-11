c3fd4f2d05da545422953f2fad9e8ac9a4d33c50
LD <dyke0005@flinders.edu.au>
2016-01-11 16:08:41 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,9 +19,31 @@ const std::string version = "v.0.0.1";
 
 namespace scan_class {
     // TYPEDEFS and MEMBER CONSTANTS
-    const scan::size_type scan::SCAN_DEFAULT_ALLOCATION;
+    const main::size_type main::MAIN_DEFAULT_ALLOCATION;
+    const scan::size_type scan::SCAN_DEFAULT_ALLOCATION;   
     
-    // CONSTRUCTORS and DESTRUCTOR   
+    // CONSTRUCTORS and DESTRUCTOR
+    main::main(size_type class_size) {
+        ct_scan = new scan[class_size];
+        st_capacity = class_size;
+        st_used = 0;
+    }
+    
+    main::~main(){
+        delete [] ct_scan;
+    }
+    
+    scan::scan(size_type class_size){
+        vt_precursor_mass = 0;
+        vt_precursor_charge = 0;
+        vt_precursor_mz = 0;
+        vt_precursor_rt = 0;
+        nt_ions = NULL;
+    }
+    
+    scan::~scan(){
+        
+    }
 }
 
 
