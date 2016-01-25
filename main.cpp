ff97b84fec86524ba0ad7ddef1d588d74448e0d7
LD <dyke0005@flinders.edu.au>
2016-01-25 11:39:12 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,72 +1,60 @@
 /*
- * File:   main.cpp
- * Author: Lukah Dykes
+ *	File:   main.cpp
+ *	Author: Lukah Dykes
  */
 
 
-/* IgCompose v.0.0.7
- *
- *
- *
- */
+ /*	IgCompose v.0.1.5
+  *
+  *
+  *
+  */
 
-#include <iostream> // provides - std::cin, std::cout
+#include <iostream> // provides - std::cin, std::cout, std::ofstream::open
 #include <string> // provides - std::string
-#include "fpf_node_h.h"
 #include "fpf_spectralsum_h.h"
 
-const std::string version = "v.0.0.7";
+
+const std::string version = "v.0.1.5";
 
 int main() {
 
-    std::cout << "-- IgCompose " << version << " --\n\n\n";
-    std::cout << "Input file?\n\n";
+	static int DEBUG_MODE = 0;
 
-    std::string input_file;
-    std::cin >> input_file;
-    std::ifstream fin_input(input_file);
+	std::cout << "-- IgCompose " << version << " --\n\n\n";
 
-    fpf_spectralsum::parse main_parse = fpf_spectralsum::parse();
-    main_parse.input_parse(fin_input, main_parse);
-    main_parse.read_parse();
+	std::cout << "Input file?\n\n";
+	std::string s_input_file;
+	std::cin >> s_input_file;
+	std::ifstream fin_input(s_input_file);
 
-    fpf_spectralsum::parse union_parse = main_parse.read_parse_union(main_parse);
-    union_parse.read_parse();
-    
-    //fpf_spectralsum::parse test_parse = fpf_spectralsum::parse();
-    //fpf_node::copy(main_parse.return_scan()->return_head_ptr(), test_parse.return_scan()->return_head_ptr(), test_parse.return_scan()->return_tail_ptr());
+	fpf_spectralsum::parse main_parse = fpf_spectralsum::parse();
+	main_parse.input_parse(fin_input, main_parse);
+	if (DEBUG_MODE >= 2) {
+		main_parse.debug_parse();
+	}
 
+	fpf_spectralsum::parse union_parse;
+	main_parse.read_parse_union(main_parse, union_parse);
+	if (DEBUG_MODE >= 2) {
+		union_parse.debug_parse();
+	}
+	if (DEBUG_MODE >= 1) {
+		union_parse.cout_mgf();
+	}
 
+	std::string s_output_file = "output.mgf";
+	std::ofstream output_fout;
+	output_fout.open(s_output_file);
+	union_parse.fout_mgf(output_fout);
 
+	std::string pong;
+	std::cout << "\n\n\nping..\n\n";
+	std::cin >> pong;
 
-    std::string pong;
-    std::cout << "\n\nping..\n\n";
-    std::cin >> pong;
-
-    return 0;
+	return 0;
 }
 
-// namespace scan_class defines object elements for fpf_scan_h 
-
-namespace fpf_spectralsum {
-
-    // TYPEDEFS and MEMBER CONSTANTS
-
-    const parse::size_type parse::PARSE_DEFAULT_ALLOCATION;
-
-    // CONSTANT MEMBER FUNCTIONS
-
-    void parse::read_parse() const {
-        for (unsigned i = 0; i < return_used(); ++i) {
-            std::cout << "\n\n" << i;
-            std::cout << "  " << nt_scan[i].return_precursor_rt();
-            std::cout << "  " << nt_scan[i].return_precursor_mass();
-            std::cout << "\n\npong!\n\n";
-            nt_scan[i].return_head_ptr()->cout_all_data(nt_scan[i].return_head_ptr());
-        }
-        if (return_used() == 0) {
-            std::cout << "\n\nNo members!";
-        }
-    };
-}
-
+//	the struggle is real ->
+//
+//	ion_union->set_fragment_ion_mz((i->return_data()->return_fragment_ion_mz());
