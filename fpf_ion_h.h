a52f39aaec24d41901bc8d6d81a6df623f58eac8
LD <dyke0005@flinders.edu.au>
2016-01-25 11:46:32 +1030

    Update fpf_ion_h.h

--- fpf_ion_h.h
+++ fpf_ion_h.h
@@ -1,6 +1,6 @@
 /*
- * File:   fpf_ion_h.h
- * Author: Lukah Dykes
+ *	File:   fpf_ion_h.h
+ *	Author: Lukah Dykes
  */
 
 
@@ -15,12 +15,14 @@ namespace fpf_ion {
     public:
         // TYPEDEFS
         typedef double value_type;
+		typedef bool bool_type;
 
         // CONSTRUCTORS
 
-        ion(const value_type& vt_init_fragment_ion_mz = value_type(), const value_type& vt_init_fragment_ion_int = value_type()) {
+        ion(const value_type& vt_init_fragment_ion_mz = value_type(), const value_type& vt_init_fragment_ion_int = value_type(), bool bt_init_union = bool_type()) {
             vt_fragment_ion_mz = vt_init_fragment_ion_mz;
             vt_fragment_ion_int = vt_init_fragment_ion_int;
+			bt_union = bt_init_union;
         };
 
         // MODIFICATION MEMBER FUNCTIONS
@@ -33,6 +35,10 @@ namespace fpf_ion {
             vt_fragment_ion_int = vt_new_fragment_ion_int;
         };
 
+		void set_union() {
+			bt_union = true;
+		};
+
         // PRIVATE MEMBER ACCESS FUNCTIONS
 
         value_type return_fragment_ion_mz() const {
@@ -43,9 +49,14 @@ namespace fpf_ion {
             return vt_fragment_ion_int;
         };
 
+		bool_type return_union() const {
+			return bt_union;
+		};
+
     private:
         value_type vt_fragment_ion_mz;
         value_type vt_fragment_ion_int;
+		bool_type bt_union;
     };
 }
 
