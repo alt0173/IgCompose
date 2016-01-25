a937bd90a4590e94fc51f5a5daff3f64510932e4
LD <dyke0005@flinders.edu.au>
2016-01-25 11:44:00 +1030

    Update fpf_node_h.h

--- fpf_node_h.h
+++ fpf_node_h.h
@@ -1,6 +1,6 @@
 /*
- * File:   fpf_node_h.h
- * Author: Lukah Dykes
+ *	File:   fpf_node_h.h
+ *	Author: Lukah Dykes
  */
 
 
@@ -9,7 +9,6 @@
 #define	FPF_NODE_H
 #include <cstdlib> // provides - size_t, NULL
 #include <iostream> // provides - std::cout
-#include "fpf_ion_h.h"
 
 namespace fpf_node {
 
@@ -17,19 +16,19 @@ namespace fpf_node {
     public:
 
         // TYPEDEFS              
-        typedef fpf_ion::ion* value_type;
+        typedef fpf_ion::ion* data_type;
         typedef node* node_type;
 
         // CONSTRUCTORS
 
-        node(const value_type& vt_init_data = value_type(), node_type nt_init_node = NULL) {
+        node(const data_type& vt_init_data = data_type(), node_type nt_init_node = NULL) {
             vt_data = vt_init_data;
             nt_node = nt_init_node;
         };
 
         // MODIFICATION MEMBER FUNCTIONS
 
-        void set_data(const value_type& vt_new_data) {
+        void set_data(const data_type& vt_new_data) {
             vt_data = vt_new_data;
         };
 
@@ -39,7 +38,7 @@ namespace fpf_node {
 
         // PRIVATE MEMBER ACCESS FUNCTIONS       
 
-        value_type return_data() const {
+        data_type return_data() const {
             return vt_data;
         };
 
@@ -51,31 +50,25 @@ namespace fpf_node {
             return nt_node;
         };
 
-        void cout_all_data(node::node_type& nt_head_ptr) const {
-            for (node::node_type link = nt_head_ptr; link != NULL; link = link->return_node()) {
-                std::cout << "\n" << link->return_data()->return_fragment_ion_mz() << "  " << link->return_data()->return_fragment_ion_int();
-            }
-        };
-
     private:
-        value_type vt_data;
+        data_type vt_data;
         node_type nt_node;
     };
     
     // FUNCTIONS
 
-    void insert_head(const node::value_type& vt_new_data, node::node_type& nt_head_ptr, node::node_type& nt_tail_ptr) {
+    void insert_head(const node::data_type& vt_new_data, node::node_type& nt_head_ptr, node::node_type& nt_tail_ptr) {
         nt_head_ptr = new node(vt_new_data, nt_head_ptr);
         if (nt_tail_ptr == NULL) {
             nt_tail_ptr = nt_head_ptr;
         }
     };
 
-    void insert_tail(const node::value_type& vt_new_data, node::node_type& nt_head_ptr, node::node_type& nt_tail_ptr) {
+    void insert_tail(const node::data_type& vt_new_data, node::node_type& nt_head_ptr, node::node_type& nt_tail_ptr) {
         node::node_type nt_new_ptr;
         nt_new_ptr = new node;
         if (nt_head_ptr == NULL) {
-            nt_head_ptr = nt_new_ptr;
+            nt_head_ptr = nt_tail_ptr;
         }
         if (nt_tail_ptr == NULL) {
             nt_tail_ptr = nt_new_ptr;
@@ -85,7 +78,7 @@ namespace fpf_node {
         nt_tail_ptr = nt_tail_ptr->return_node();
     };
 
-    void insert(const node::value_type& vt_new_data, node::node_type& nt_prev_ptr) {
+    void insert(const node::data_type& vt_new_data, node::node_type& nt_prev_ptr) {
         // case of empty list?
         node::node_type nt_new_ptr;
         nt_new_ptr = new node;
