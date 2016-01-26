8b901691f5b72f744934887c815409465a225b87
LD <dyke0005@flinders.edu.au>
2016-01-26 15:24:40 +1030

    Update fpf_ion_h.h

--- fpf_ion_h.h
+++ fpf_ion_h.h
@@ -14,48 +14,48 @@ namespace fpf_ion {
     class ion {
     public:
         // TYPEDEFS
-        typedef double value_type;
+        typedef long double value_type;
 		typedef bool bool_type;
 
         // CONSTRUCTORS
 
         ion(const value_type& vt_init_fragment_ion_mz = value_type(), const value_type& vt_init_fragment_ion_int = value_type(), bool bt_init_union = bool_type()) {
             vt_fragment_ion_mz = vt_init_fragment_ion_mz;
-            vt_fragment_ion_int = vt_init_fragment_ion_int;
+            vt_fragment_ion_intensity = vt_init_fragment_ion_int;
 			bt_union = bt_init_union;
         };
 
         // MODIFICATION MEMBER FUNCTIONS
 
-        void set_fragment_ion_mz(const value_type& vt_new_fragment_ion_mz) {
+		inline void set_fragment_ion_mz_vt(const value_type& vt_new_fragment_ion_mz) {
             vt_fragment_ion_mz = vt_new_fragment_ion_mz;
         };
 
-        void set_fragment_ion_int(const value_type& vt_new_fragment_ion_int) {
-            vt_fragment_ion_int = vt_new_fragment_ion_int;
+		inline void set_fragment_ion_intensity_vt(const value_type& vt_new_fragment_ion_int) {
+            vt_fragment_ion_intensity = vt_new_fragment_ion_int;
         };
 
-		void set_union() {
+		inline void set_union_b() {
 			bt_union = true;
 		};
 
         // PRIVATE MEMBER ACCESS FUNCTIONS
 
-        value_type return_fragment_ion_mz() const {
+		inline value_type return_fragment_ion_mz() const {
             return vt_fragment_ion_mz;
         };
 
-        value_type return_fragment_ion_int() const {
-            return vt_fragment_ion_int;
+        inline value_type return_fragment_ion_intensity() const {
+            return vt_fragment_ion_intensity;
         };
 
-		bool_type return_union() const {
+		inline bool_type return_union() const {
 			return bt_union;
 		};
 
     private:
         value_type vt_fragment_ion_mz;
-        value_type vt_fragment_ion_int;
+        value_type vt_fragment_ion_intensity;
 		bool_type bt_union;
     };
 }
