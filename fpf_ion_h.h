f6a4620a6124b0077b177d07a4cf6e5de09c8897
LD <dyke0005@flinders.edu.au>
2016-01-28 14:41:23 +1030

    Update fpf_ion_h.h

--- fpf_ion_h.h
+++ fpf_ion_h.h
@@ -2,7 +2,7 @@
  *	File:   fpf_ion_h.h
  *	Author: Lukah Dykes
  */
- 
+
 
 
 #ifndef FPF_ION_H
@@ -15,33 +15,39 @@ namespace fpf_ion {
     public:
         // TYPEDEFS
         typedef long double value_type;
-		typedef bool bool_type;
+        typedef size_t size_type;
+        typedef bool bool_type;
 
         // CONSTRUCTORS
 
-        ion(const value_type& vt_init_fragment_ion_mz = value_type(), const value_type& vt_init_fragment_ion_int = value_type(), bool bt_init_union = bool_type()) {
+        ion(const value_type& vt_init_fragment_ion_mz = value_type(), const value_type& vt_init_fragment_ion_int = value_type(), bool bt_init_union = bool_type(), bool bt_init_union_init = bool_type()) {
             vt_fragment_ion_mz = vt_init_fragment_ion_mz;
             vt_fragment_ion_intensity = vt_init_fragment_ion_int;
-			bt_union = bt_init_union;
+            bt_union = bt_init_union;
+            bt_union_init = bt_init_union_init;
         };
 
         // MODIFICATION MEMBER FUNCTIONS
 
-		inline void set_fragment_ion_mz_vt(const value_type& vt_new_fragment_ion_mz) {
+        inline void set_fragment_ion_mz_vt(const value_type& vt_new_fragment_ion_mz) {
             vt_fragment_ion_mz = vt_new_fragment_ion_mz;
         };
 
-		inline void set_fragment_ion_intensity_vt(const value_type& vt_new_fragment_ion_int) {
+        inline void set_fragment_ion_intensity_vt(const value_type& vt_new_fragment_ion_int) {
             vt_fragment_ion_intensity = vt_new_fragment_ion_int;
         };
 
-		inline void set_union_b() {
-			bt_union = true;
-		};
+        inline void set_union_b() {
+            bt_union = true;
+        };
+        
+        inline void set_union_init_b() {
+            bt_union_init = true;
+        };
 
         // PRIVATE MEMBER ACCESS FUNCTIONS
 
-		inline value_type return_fragment_ion_mz() const {
+        inline value_type return_fragment_ion_mz() const {
             return vt_fragment_ion_mz;
         };
 
@@ -49,14 +55,19 @@ namespace fpf_ion {
             return vt_fragment_ion_intensity;
         };
 
-		inline bool_type return_union() const {
-			return bt_union;
-		};
+        inline bool_type return_union() const {
+            return bt_union;
+        };
+        
+        inline bool_type return_union_init() const {
+            return bt_union_init;
+        };
 
     private:
         value_type vt_fragment_ion_mz;
         value_type vt_fragment_ion_intensity;
-		bool_type bt_union;
+        bool_type bt_union;
+        bool_type bt_union_init;
     };
 }
 
