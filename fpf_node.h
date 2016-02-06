17bb0887850cd59cbf5d331177e45a1f4d99e368
LD <dyke0005@flinders.edu.au>
2016-02-06 18:14:04 +1030

    Update fpf_node.h

--- fpf_node.h
+++ fpf_node.h
@@ -40,7 +40,7 @@ namespace fpf_node {
 		//
 
 
-		void set_data_d(const data_type& d_new_data) {
+		void set_data_dt(const data_type& d_new_data) {
 			d_data = d_new_data;
 		};
 		//
@@ -64,7 +64,7 @@ namespace fpf_node {
 		//
 
 
-		data_type return_data_dt() const {
+		data_type dt_return_data() const {
 			return d_data;
 		};
 		//
@@ -109,51 +109,53 @@ namespace fpf_node {
 
 
 	template <class element>
-	void list_insert_head(const typename node<element>::data_type& d_new_data, typename node<element>::node_type& nt_ion_head_ptr, typename node<element>::node_type& nt_ion_tail_ptr) {
+	void list_insert_head(const typename node<element>::data_type dt_new_data, typename node<element>::node_type& nt_node_head_ptr, typename node<element>::node_type& nt_node_tail_ptr) {
 		node<element>::node_type nt_new_ptr = new node;
-		nt_new_ptr->set_data_d(d_new_data);
-		if (nt_ion_head_ptr == NULL) {
-			nt_ion_head_ptr = nt_new_ptr;
+		nt_new_ptr->set_data_dt(dt_new_data);
+		if (nt_node_head_ptr == NULL) {
+			nt_node_head_ptr = nt_new_ptr;
 		}
-		if (nt_ion_tail_ptr == NULL) {
-			nt_ion_tail_ptr = nt_new_ptr;
+		if (nt_node_tail_ptr == NULL) {
+			nt_node_tail_ptr = nt_new_ptr;
 		}
-		nt_new_ptr->set_up_node_nt(nt_ion_head_ptr);
-		nt_ion_head_ptr->set_down_node_nt(nt_new_ptr);
-		nt_ion_head_ptr = nt_ion_head_ptr->return_down_node_nt();
+		nt_new_ptr->set_up_node_nt(nt_node_head_ptr);
+		nt_node_head_ptr->set_down_node_nt(nt_new_ptr);
+		nt_node_head_ptr = nt_node_head_ptr->return_down_node_nt();
 	};
 	//
 	//
 
 	template <class element>
-	void list_insert_tail(const typename node<element>::data_type& d_new_data, typename node<element>::node_type& nt_ion_head_ptr, typename node<element>::node_type& nt_ion_tail_ptr) {
+	void list_insert_tail(const typename node<element>::data_type& dt_new_data, typename node<element>::node_type& nt_node_head_ptr, typename node<element>::node_type& nt_node_tail_ptr) {
 		node<element>::node_type nt_new_ptr = new node<element>;
-		nt_new_ptr->set_data_d(d_new_data);
-		if (nt_ion_head_ptr == NULL) {
-			nt_ion_head_ptr = nt_new_ptr;
+		nt_new_ptr->set_data_dt(dt_new_data);
+		if (nt_node_head_ptr == NULL) {
+			nt_node_head_ptr = nt_new_ptr;
 		}
-		if (nt_ion_tail_ptr == NULL) {
-			nt_ion_tail_ptr = nt_new_ptr;
+		if (nt_node_tail_ptr == NULL) {
+			nt_node_tail_ptr = nt_new_ptr;
 		}
-		nt_new_ptr->set_down_node_nt(nt_ion_tail_ptr);
-		nt_ion_tail_ptr->set_up_node_nt(nt_new_ptr);
-		nt_ion_tail_ptr = nt_ion_tail_ptr->return_up_node_nt();
+		else {
+			nt_new_ptr->set_down_node_nt(nt_node_tail_ptr);
+			nt_node_tail_ptr->set_up_node_nt(nt_new_ptr);
+			nt_node_tail_ptr = nt_new_ptr;
+		}
 	};
 	//
 	//
 
 	template <class element>
-	void list_insert_up(const typename node<element>::data_type& d_new_data, typename node<element>::node_type& nt_current_ptr, typename node<element>::node_type& nt_ion_head_ptr, typename node<element>::node_type& nt_ion_tail_ptr) {
-		if (nt_current_ptr != nt_ion_tail_ptr) {
+	void list_insert_up(const typename node<element>::data_type& dt_new_data, typename node<element>::node_type& nt_current_ptr, typename node<element>::node_type& nt_node_head_ptr, typename node<element>::node_type& nt_node_tail_ptr) {
+		if (nt_current_ptr != nt_node_tail_ptr) {
 			node<element>::node_type nt_new_ptr = new node<element>;
-			nt_new_ptr->set_data_d(d_new_data);
+			nt_new_ptr->set_data_dt(dt_new_data);
 			nt_current_ptr->return_up_node_nt()->set_down_node_nt(nt_new_ptr);
 			nt_new_ptr->set_up_node_nt(nt_current_ptr->return_up_node_nt());
 			nt_new_ptr->set_down_node_nt(nt_current_ptr);
 			nt_current_ptr->set_up_node_nt(nt_new_ptr);
 		}
 		else {
-			list_insert_tail<element>(d_new_data, nt_ion_head_ptr, nt_ion_tail_ptr);
+			list_insert_tail<element>(dt_new_data, nt_node_head_ptr, nt_node_tail_ptr);
 		}
 	};
 	//
@@ -166,10 +168,10 @@ namespace fpf_node {
 	//	}
 	//	nt_new_head_ptr = NULL;
 	//	nt_new_tail_ptr = NULL;
-	//	list_insert_head(nt_call_ptr->return_data_dt(), nt_new_head_ptr, nt_new_tail_ptr);
+	//	list_insert_head(nt_call_ptr->dt_return_data(), nt_new_head_ptr, nt_new_tail_ptr);
 	//	nt_call_ptr = nt_call_ptr->return_up_node_nt();
 	//	while (nt_call_ptr != NULL) {
-	//		list_insert_tail(nt_call_ptr->return_data_dt(), nt_new_head_ptr, nt_new_tail_ptr);
+	//		list_insert_tail(nt_call_ptr->dt_return_data(), nt_new_head_ptr, nt_new_tail_ptr);
 	//		nt_new_tail_ptr = nt_new_tail_ptr->return_up_node_nt();
 	//		nt_call_ptr = nt_call_ptr->return_up_node_nt();
 	//	}
@@ -179,7 +181,7 @@ namespace fpf_node {
 	void list_remove_head(typename node<element>::node_type& nt_ion_head_ptr) {
 		node<element>::node_type nt_del_ptr;
 		nt_del_ptr = nt_ion_head_ptr;
-		nt_ion_head_ptr = nt_del_ptr->return_up_node_nt();
+		nt_ion_head_ptr = nt_ion_head_ptr->return_up_node_nt();
 		nt_ion_head_ptr->set_down_node_nt(NULL);
 		delete nt_del_ptr;
 	}
