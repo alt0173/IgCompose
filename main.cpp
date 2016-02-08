4debf6557f45c7dfd489497cbcea7c4a7825072c
LD <dyke0005@flinders.edu.au>
2016-02-08 11:56:11 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
  */
 
 
- /*	IgCompose v.6.4.0
+ /*	IgCompose v.6.8.0
   *
   *
   *
   */
- 
+
+
 #include <cstdlib> // provides - EXIT_SUCCESS, size_t, NULL
 #include <iostream> // provides - std::cin, std::cout, std::ofstream::open
 #include <string> // provides - std::string
@@ -17,33 +18,31 @@
 #include "fpf_spectralsum.h"
 
 
-const std::string version = "v.6.4.0";
-
 static int DEBUG_MODE = int();
 
 int main() {
 
-	std::cout << "-- IgCompose " << version << " --\n\n\n";
+	fpf_spectralsum::display_interface();
 
-	std::cout << "Input file?\n\n";
+	std::cout << "\n\nInput file?\n\n";
 	std::string s_input_file;
 	std::cin >> s_input_file;
 	std::ifstream fin_input(s_input_file);
 
-	std::cout << "\n\nInput file?\n\n";
-	std::string s_input_file_2;
-	std::cin >> s_input_file_2;
-	std::ifstream fin_input_2(s_input_file_2);
+	//std::cout << "\n\nInput file?\n\n";
+	//std::string s_input_file_2;
+	//std::cin >> s_input_file_2;
+	//std::ifstream fin_input_2(s_input_file_2);
 
-	std::cout << "\n\nInput file?\n\n";
-	std::string s_input_file_3;
-	std::cin >> s_input_file_3;
-	std::ifstream fin_input_3(s_input_file_3);
+	//std::cout << "\n\nInput file?\n\n";
+	//std::string s_input_file_3;
+	//std::cin >> s_input_file_3;
+	//std::ifstream fin_input_3(s_input_file_3);
 
-	std::cout << "\n\nInput file?\n\n";
-	std::string s_input_file_4;
-	std::cin >> s_input_file_4;
-	std::ifstream fin_input_4(s_input_file_4);
+	//std::cout << "\n\nInput file?\n\n";
+	//std::string s_input_file_4;
+	//std::cin >> s_input_file_4;
+	//std::ifstream fin_input_4(s_input_file_4);
 
 	std::cout << "\n\ndebug mode?\n\n";
 	std::cin >> DEBUG_MODE;
@@ -52,7 +51,7 @@ int main() {
 
 	fpf_spectralsum::mgf* init_mgf = new fpf_spectralsum::mgf();
 	init_mgf->input_parse(fin_input, init_mgf);
-	init_mgf->input_parse(fin_input_2, init_mgf);
+	//init_mgf->input_parse(fin_input_2, init_mgf);
 	//init_mgf->input_parse(fin_input_3, init_mgf);
 	//init_mgf->input_parse(fin_input_4, init_mgf);
 	if (DEBUG_MODE == 4) {
@@ -76,6 +75,8 @@ int main() {
 
 	std::string pong;
 	std::cout << "\n\n - \n - \n - ";
+	std::cin >> pong;
+	std::cout << "Check count? - ";
 	std::cin >> pong;
 
 	return EXIT_SUCCESS;
